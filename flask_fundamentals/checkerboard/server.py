b26bf8e27af079cb31a24854878000bd4c6e15da
SaeedQatanani <saeedqatananil@gmail.com>
2022-09-18 14:45:18 +0300

    edited

--- flask_fundamentals/checkerboard/server.py
+++ flask_fundamentals/checkerboard/server.py
@@ -3,20 +3,20 @@ app = Flask(__name__)
 
 # Each route has its own html elements.
 @app.route('/')
-def rigid_board(index=0):
-    return render_template('index.html', currentindex = index)
+def rigid_board():
+    return render_template('index.html', rows = int(8), columns = int(8), firstcolor = 'red', secondcolor = 'black')
 
 @app.route('/<int:x>')
-def semi_flexiable_board(index=1, x=0):
-    return render_template('index.html', rows = x, currentindex = index)
+def semi_flexiable_board(x=0):
+    return render_template('index.html', rows = x, columns = 8, firstcolor = 'red', secondcolor = 'black')
 
 @app.route('/<int:x>/<int:y>')
-def flexiable_board(index=2, x=0, y=0):
-    return render_template('index.html', rows = x, columns = y, currentindex = index)
+def flexiable_board(x=0, y=0):
+    return render_template('index.html', rows = x, columns = y, firstcolor = 'red', secondcolor = 'black')
 
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
-def color_flexiable_board(index=3, x=0, y=0, color1 = 'red', color2 = 'black'):
-    return render_template('index.html', rows = x, columns = y, firstcolor = color1, secondcolor = color2, currentindex = index)
+def color_flexiable_board(x=0, y=0, color1 = 'red', color2 = 'black'):
+    return render_template('index.html', rows = x, columns = y, firstcolor = color1, secondcolor = color2)
 
 if __name__ == '__main__':
     app.run(debug=True)
