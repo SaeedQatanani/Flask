918f27cc1a4724e27135572debe4d8aaf50d6a71
SaeedQatanani <saeedqatananil@gmail.com>
2022-09-18 14:08:38 +0300

    practice

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -20,5 +20,14 @@ def show_user_profile(username, id):
     print(id)
     return "username: " + username + ", id: " + id
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name' : 'Saeed', 'age' : 23},
+        {'name': 'Hasan', 'age': 24},
+        {'name': 'Osama', 'age': 22}
+    ]
+    return render_template('lists.html', random_numbers = [3,1,5], students = student_info)
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
